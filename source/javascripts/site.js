e01a1bf5e4571e4ce61a9f87f11d30028847e6e5
stephanie <hayden.159@osu.edu>
2017-04-09 22:01:23 -0400

    converted to jquery, made the tags resort upon click

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,61 +1,56 @@
 
 
-var selection = [];
+var selection = []; 
 
-var project_children = document.getElementById("projects").children;
-var project_list = Array.prototype.slice.call(project_children);
-var projects_to_move = [];
+var project_children = $("#projects").children();
+var project_list = jQuery.makeArray(project_children); 
 
-var tags = document.querySelectorAll("span.skill"); //skills tags at the top of the page
+var tags = $("span.skill"); //skills tags at the top of the page
+
 
 
 
 // function to sort code projects based on selected tags
 function sortProjects() {
-	for (i=0; i<project_list.length; i++) { //iterate through projects
-		var project = project_list[i];
-		console.log("project: " + project.toString());
-		var span_tags = project.getElementsByTagName('span'); //get all spans in the project
-		for(j=0; j<span_tags.length; j++) {
-			var tag = span_tags[j].innerHTML;
-			if (selection.indexOf(tag)>=0) { //if span tag matches selected span tag, add it to to_move
-				if (!projects_to_move[project]) { //project not in to_move
-					projects_to_move.push({id:project, count:0});
-				} else { //project already in to_move
-					var popular_project = projects_to_move[project]; //find project
-					projects_to_move[popular_project].count++;//add one to its number field 
-				}
-			}
+	var projects_to_move;
+	for(i=0; i<selection.length; i++){
+		var span_matches = $("#projects").find("span").filter(":contains(" + selection[i] + ")");
+		if (i==0){
+			projects_to_move = span_matches.parent();
+		} else {
+			projects_to_move = projects_to_move.add(span_matches.parent()); //future--make span_matches.parent a previous value
 		}
 	}
+
+	//put projects at the top
+	var moved;
+	projects_to_move.detach();
+	projects_to_move.each( function(index, value) {
+		$("#projects").prepend(value);
 	
-	//put projects_to_move at the top
-	for (i=0; i<projects_to_move.length; i++){
-		project_children.unshift(projects_to_move[i]);
-	}
+	});
 	
 }
 
-
 // click function for selector tags
 function addSelection() {
-	if (selection.indexOf(this.innerHTML) >= 0) {
-		selection.splice(selection.indexOf(this.innerHTML), 1);
-		this.classList.remove('active');
-		sortProjects();
+	if (selection.indexOf($(this).text()) >= 0) { //project already selected
+		selection.splice(selection.indexOf($(this).text()), 1);
+		$(this).removeClass("active");
 	}	
-	else {
-		selection.push(this.innerHTML);
-		this.className += ' active';
-		sortProjects();
+	else { //project newly selected
+		selection.push($(this).text());
+		$(this).addClass("active");
+
 	}
 	console.log(selection.toString());
+	sortProjects();
 }
 
 
 
 //add onclick function
-for(var j = 0; j < tags.length; j++) {
-    	tags[j].addEventListener("click", addSelection);
-}
+tags.on('click', addSelection);
+
+
 
